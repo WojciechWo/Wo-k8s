a5543b7c14da245db5dd6121c98a5f2513ed863b
Wołoszyn Wojciech <wojtek201990@gmail.com>
2023-01-03 19:00:36 +0100

    Initial commit

---
+++ src/main/java/com/WojciechWo/Wok8s/WoK8sApplication.java
@@ -0,0 +1,13 @@
+package com.WojciechWo.Wok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WojciechWo/Wok8s/WoK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WojciechWo.Wok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
